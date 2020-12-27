2454052b148edbfbb4d58c4fdcd27600ee3a5907
mcg25035 <70533278+mcg25035@users.noreply.github.com>
2020-12-27 16:25:49 +0800

    Create (202012271625)index.php

---
+++ (202012271625)index.php
@@ -0,0 +1,10 @@
+<html><head><title>CityofUniverse應徵系統 - 上傳</title></head><body>
+<body bgcolor="white"><center>
+<big><big><p style="background-color:white;color:blue;">City of Universe應徵系統 - 上傳</p></big></big>
+<p style="background-color:white;color:red;">*檔案大小需小於2MB*</p>
+<form method="post" enctype="multipart/form-data" action="upload.php">
+  <input type="file" name="my_file",style="background-color:white;">
+  <input type="submit" value="上傳">
+</form></center><center>
+<div style="position:absolute; bottom:0;"><small>Copyright ©2005-2135 CodingBear_TW All rights reserved</small></div></center>
+</body></html>
